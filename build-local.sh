912fcbf7b8e7061715f0a0e1b0fc813a37cb34ab
Nick Bartos <flamingo@2thebatcave.com>
2019-04-23 19:06:30 -0700

    foo

--- build-local.sh
+++ build-local.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
+flatpak --user uninstall -y org.sparkleshare.SparkleShare || true
+flatpak --user remote-delete sparkleshare-local || true
+
 rm -Rf ./repo
+
 flatpak-builder --force-clean --repo=./repo ./app org.sparkleshare.SparkleShare.local.yml
-flatpak --user uninstall -y org.sparkleshare.SparkleShare; flatpak --user remote-delete sparkleshare-local
 flatpak --user remote-add --no-gpg-verify sparkleshare-local ./repo
 flatpak --user install -y sparkleshare-local org.sparkleshare.SparkleShare
